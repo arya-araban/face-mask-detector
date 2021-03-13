7a6bd42c288c720c3bd0024c5ef4ca6e671fecfd
Arya Araban <arya.araban21@gmail.com>
2021-03-13 22:55:05 +0330

    full mask detector using preTrained model

--- face-detector.py
+++ face-detector.py
@@ -1,11 +1,27 @@
 import cv2
-import sys
-import keyboard
+import tensorflow as tf
+import numpy as np
+from keras_preprocessing.image import img_to_array
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+
+
+def preprocess_image(img):
+    im = cv2.resize(img, (224, 224))
+    im = img_to_array(im)
+    im = np.expand_dims(im, axis=0)
+    im = preprocess_input(im)
+    return im
+
+
+FRAME_SKIPS = 20
+cur_frame = 0
+
+loaded_model = tf.keras.models.load_model('mask_recog.h5')
 
 # considering what to print
 whatToPrint = 'No Mask'
-font = cv2.FONT_ITALIC
 color = (0, 0, 255)
+font = cv2.FONT_ITALIC
 
 cascPath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascPath)
@@ -30,7 +46,7 @@ while True:
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
         crop_img = frame[y:y + h, x:x + w]
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.rectangle(frame, (x, y - 50), (x + w, y + h + 50), (0, 255, 0), 2)
     # show text for the status of wearing - not wearing mask
     cv2.putText(frame, whatToPrint, (10, 450), font, 1, color, 2, cv2.LINE_AA)
     # Display the resulting frame
@@ -39,10 +55,29 @@ while True:
 
     if pressedKey == ord('q'):
         break
+
+    if cur_frame % FRAME_SKIPS == 0:
+        cur_frame = 0
+
+        try:
+            crop_img = preprocess_image(crop_img)
+            prediction = loaded_model.predict(crop_img)
+        except:
+            pass
+
+        if (np.argmax(prediction) == 1):
+            whatToPrint = 'No Mask'
+            color = (0, 0, 255)
+        else:
+            whatToPrint = "mask"
+            color = (0, 255, 0)
+
     if pressedKey == ord("p"):
-        cv2.imshow("cropped", crop_img)
-        whatToPrint = "mask"
-        color = (0, 255, 0)
+        crop_img = preprocess_image(crop_img)
+        prediction = loaded_model.predict(crop_img)
+        print(prediction)
+
+    cur_frame += 1
 # When everything is done, release the capture
 video_capture.release()
 cv2.destroyAllWindows()
